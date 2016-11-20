7a769e12462762a8b713e393a470dab30c73948e
Khai Phan <Khai Phan>
2016-11-20 14:59:28 -0500

    another attempt at fixing the timeout

--- js/client.js
+++ js/client.js
@@ -102,7 +102,7 @@ function main(){
         }
     });
 
-    setInterval(function(){ws.send({type: 'ping'})},10000);
+    setInterval(function(){ws.send({type: 'ping'}); console.log("This is happening");},3000);
 
 }
 
